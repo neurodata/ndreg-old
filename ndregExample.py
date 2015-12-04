74e6aa666e7a3e7ee5a197e590b1c234f2c5e4c8
kkutten1 <kkutten1@users.noreply.github.com>
2015-12-04 18:54:36 -0500

    Delete ndregExample.py

--- ndregExample.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from ndreg import *
-inPath = "inImage.img"
-refPath = "refImage.img"
-outDirPath = "./"
-
-imgRegistration(inPath, refPath, outDirPath, True)
