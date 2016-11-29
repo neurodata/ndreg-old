91d2d7e0a5e4cd84fd6c53ffc3ccd9c75f1f9741
Kwame Kutten <kkutten1@jhmi.edu>
2016-11-29 14:54:25 -0500

    Added script to ramonify ARA.

---
+++ doc/ramonifyARA.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import json
+from ndreg import *
+import ndio.ramon as ndramon
+import ndio.remote.neurodata as neurodata
+"""
+Here we show how to RAMONify Allen Reference Atlas data.
+First we download annotation ontology from Allen Brain Atlas API.
+It returns a JSON tree in which larger parent structures are divided into smaller children regions.
+For example the "corpus callosum" parent is has children "corpus callosum, anterior forceps", "genu of corpus callosum", "corpus callosum, body", etc
+"""
+
+url = "http://api.brain-map.org/api/v2/structure_graph_download/1.json"
+jsonRaw = requests.get(url).content
+jsonDict = json.loads(jsonRaw)
+
+"""
+Next we collect the names and ids of all of the regions.
+Since our json data is a tree we can walk through it in arecursive manner.
+Thus starting from the root...
+"""
+root = jsonDict['msg'][0]
+"""
+...we define a recursive function ...
+"""
+def getChildrenNames(parent, childrenNames={}):
+    for childIndex in range(len(parent['children'])):
+        child = parent['children'][childIndex]
+        childrenNames[child['id']] = child['name']
+
+        childrenNames = getChildrenNames(child, childrenNames)
+    return childrenNames
+
+"""
+... and collect all of the region names in a dictionary with the "id" field as keys.
+"""
+regionDict = getChildrenNames(root)
+"""
+Next we RAMONify the data
+"""
+token = "ara_ccf3"
+channel = "annotation2"
+nd = neurodata()
+
+for regionId in regionDict.keys():
+    regionName = regionDict[regionId]
+
+    kvpairs = {'name': regionName}
+    ramonObj = ndramon.RAMONGeneric(id=regionId, resolution=0, kvpairs=kvpairs)
+    try:
+        nd.post_ramon(token, channel, ramonObj)
+        print "Successfully posted ramon obj {0} for {1}".format(regionId, regionName)
+    except:
+        print "Failed to post ramon obj {0} for {1}".format(regionId, regionName)
